d486c74cca0903bc294c85ffda84caeb202c5826
tomaszosk <t.oskroba@icloud.com>
2020-04-02 17:22:19 +0200

    Triple Tap Gesture added

--- CircleSpawn/CircleSpawn/CircleSpawnController.swift
+++ CircleSpawn/CircleSpawn/CircleSpawnController.swift
@@ -2,35 +2,55 @@ import UIKit
 
 class CircleSpawnController: UIViewController {
 
-	// TODO: Assignment 1
+    // TODO: Assignment 1
 
-	override func loadView() {
-		view = UIView()
-		view.backgroundColor = .white
-        
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        tap.numberOfTapsRequired = 2
-        view.addGestureRecognizer(tap)
-	}
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemBackground
     
-    @objc func handleTap(_ tap: UITapGestureRecognizer) {
-           
-        //Tworzenie koła
-        let size: CGFloat = 100
-        let circleView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: size, height: size)))
-        circleView.center = tap.location(in: self.view)
-        circleView.backgroundColor = UIColor.randomBrightColor()
-        circleView.layer.cornerRadius = size * 0.5
-        circleView.alpha = 0;
-        view.addSubview(circleView)
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(sender:)))
+        doubleTap.numberOfTapsRequired = 2
+        view.addGestureRecognizer(doubleTap)
         
-        //Animacja dodania koła
-        circleView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
-        let animator = UIViewPropertyAnimator(duration: 0.5 , curve: .easeOut, animations: {
-            circleView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            circleView.alpha = 1;
-        })
-        animator.startAnimation()
+        let tripleTap = UITapGestureRecognizer(target: self, action: #selector(handleTripleTap(sender:)))
+        tripleTap.numberOfTapsRequired = 3
+        view.addGestureRecognizer(tripleTap)
+        
+        doubleTap.require(toFail: tripleTap)
+    }
+    
+    @objc func handleDoubleTap(sender: UITapGestureRecognizer) {
+           
+        if sender.state == .ended {
+//            print("Double tap gesture")
+            //Tworzenie koła
+            let size: CGFloat = 100
+            let circleView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: size, height: size)))
+            circleView.center = sender.location(in: self.view)
+            circleView.backgroundColor = UIColor.randomBrightColor()
+            circleView.layer.cornerRadius = size * 0.5
+            circleView.alpha = 0;
+            circleView.tag = 100;
+            view.addSubview(circleView)
+            
+            //Animacja dodania koła
+            circleView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            let animator = UIViewPropertyAnimator(duration: 0.1 , curve: .easeInOut, animations: {
+                circleView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                circleView.alpha = 1;
+            })
+            animator.startAnimation()
+        }
+    }
+    
+    @objc func handleTripleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            if let viewWithTag = self.view.viewWithTag(100) {
+                viewWithTag.removeFromSuperview()
+            } else {
+                print("No!")
+            }
+        }
     }
 }
 
